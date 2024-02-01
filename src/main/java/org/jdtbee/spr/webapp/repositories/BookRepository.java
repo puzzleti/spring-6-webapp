530febdabb1af6e4b558bd6c12acfe8a74846dc3
Thomas Immen <thimmen@mail.de>
2024-02-01 16:31:03 +0100

    repositories annotated

--- src/main/java/org/jdtbee/spr/webapp/repositories/BookRepository.java
+++ src/main/java/org/jdtbee/spr/webapp/repositories/BookRepository.java
@@ -2,7 +2,9 @@ package org.jdtbee.spr.webapp.repositories;
 
 import org.jdtbee.spr.webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 
 }
--- src/main/java/org/jdtbee/spr/webapp/repositories/PublisherRepository.java
+++ src/main/java/org/jdtbee/spr/webapp/repositories/PublisherRepository.java
@@ -2,7 +2,9 @@ package org.jdtbee.spr.webapp.repositories;
 
 import org.jdtbee.spr.webapp.domain.Publisher;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 
 }
